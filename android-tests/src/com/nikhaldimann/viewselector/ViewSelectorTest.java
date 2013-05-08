a7857dea35f2fbffce1690ef01e50324478b99ff
Nik Haldimann <nhaldimann@gmail.com>
2013-05-08 11:21:36 -0400

    Initial project structure with library and android test project

---
+++ android-tests/src/com/nikhaldimann/viewselector/ViewSelectorTest.java
@@ -0,0 +1,12 @@
+package com.nikhaldimann.viewselector;
+
+import junit.framework.TestCase;
+
+public class ViewSelectorTest extends TestCase {
+
+    public void testConstructor() {
+        ViewSelector selector = new ViewSelector();
+        assertTrue(true);
+    }
+
+}
---
+++ lib/src/main/java/com/nikhaldimann/viewselector/ViewSelector.java
@@ -0,0 +1,8 @@
+package com.nikhaldimann.viewselector;
+
+import se.fishtank.css.selectors.scanner.Scanner;
+
+public class ViewSelector {
+
+    private Scanner scanner;
+}
