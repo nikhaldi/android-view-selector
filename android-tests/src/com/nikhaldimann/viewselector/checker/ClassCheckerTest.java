5921826453071eb7e8970ccda943aaef27fc958b
Nik Haldimann <nhaldimann@gmail.com>
2013-05-08 16:31:15 -0400

    Add descendant checking

--- android-tests/src/com/nikhaldimann/viewselector/checker/ClassCheckerTest.java
+++ android-tests/src/com/nikhaldimann/viewselector/checker/ClassCheckerTest.java
@@ -8,6 +8,8 @@ import se.fishtank.css.selectors.Selector;
 import se.fishtank.css.selectors.Selector.Combinator;
 import android.test.AndroidTestCase;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ClassCheckerTest extends AndroidTestCase {
@@ -23,6 +25,22 @@ public class ClassCheckerTest extends AndroidTestCase {
         assertContentsInOrder(check("FooView", Combinator.DESCENDANT, view));
         assertContentsInOrder(check("TextView", Combinator.DESCENDANT, view), view);
         assertContentsInOrder(check("*", Combinator.DESCENDANT, view), view);
+    }
+
+    public void testViewGroupWithDescendants() {
+        LinearLayout layout = new LinearLayout(getContext());
+        TextView view1 = new TextView(getContext());
+        TextView view2 = new TextView(getContext());
+        layout.addView(view1);
+        layout.addView(view2);
+        FrameLayout layout2 = new FrameLayout(getContext());
+        TextView view3 = new TextView(getContext());
+        layout2.addView(view3);
+        layout.addView(layout2);
+        assertContentsInOrder(check("FooView", Combinator.DESCENDANT, layout));
+        assertContentsInOrder(check("LinearLayout", Combinator.DESCENDANT, layout), layout);
+        assertContentsInOrder(check("FrameLayout", Combinator.DESCENDANT, layout), layout2);
+        assertContentsInOrder(check("TextView", Combinator.DESCENDANT, layout), view1, view2, view3);
     }
 
 }
--- lib/src/main/java/com/nikhaldimann/viewselector/checker/ClassChecker.java
+++ lib/src/main/java/com/nikhaldimann/viewselector/checker/ClassChecker.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import se.fishtank.css.selectors.Selector;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Checks views based on matching the tag from a selector to the class of
@@ -20,13 +21,25 @@ public class ClassChecker implements ViewTraversalChecker {
 
     public List<View> check(List<View> views) {
         List<View> result = new ArrayList<View>();
-        String className = selector.getTagName();
         for (View view : views) {
-            if (matchesClass(className, view)) {
-                result.add(view);
-            }
+            checkDescendants(view, result);
         }
         return result;
+    }
+
+    private void checkDescendants(View view, List<View> result) {
+        // Note: Using recursion. We hope that real-world layouts don't get deep
+        // enough that this causes a problem.
+        String className = selector.getTagName();
+        if (matchesClass(className, view)) {
+            result.add(view);
+        } else if (view instanceof ViewGroup) {
+            ViewGroup group = (ViewGroup) view;
+            for (int i = 0; i < group.getChildCount(); i++) {
+                View childView = group.getChildAt(i);
+                checkDescendants(childView, result);
+            }
+        }
     }
 
     private boolean matchesClass(String className, View view) {
