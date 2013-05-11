4c97509040d3a346fe5e2989f33a4d543eefba94
Nik Haldimann <nhaldimann@gmail.com>
2013-05-11 15:09:01 -0400

    Convert ViewSelectorAssertsTest to new test sharing scheme

--- src/android-test/src/com/nikhaldimann/viewselector/ViewSelectorAssertsTest.java
+++ src/android-test/src/com/nikhaldimann/viewselector/ViewSelectorAssertsTest.java
@@ -1,75 +1,13 @@
 package com.nikhaldimann.viewselector;
 
-import static com.nikhaldimann.viewselector.ViewSelectorAsserts.assertViewAttributesEqual;
-import static com.nikhaldimann.viewselector.ViewSelectorAsserts.assertViewCount;
-import static com.nikhaldimann.viewselector.ViewSelectorAsserts.assertViewExists;
+import com.nikhaldimann.viewselector.test.abstrakt.AbstractViewSelectorAssertsTest;
+import com.nikhaldimann.viewselector.test.util.ViewFactory;
+import com.nikhaldimann.viewselector.testutil.AndroidTestViewFactory;
 
-import android.test.AndroidTestCase;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.TextView;
+public class ViewSelectorAssertsTest extends AbstractViewSelectorAssertsTest {
 
-public class ViewSelectorAssertsTest extends AndroidTestCase {
-
-    /**
-     * Fails with a RuntimeException, so we can distinguish this from planned
-     * assertion failures.
-     */
-    private void failHard() {
-        throw new RuntimeException("Failed to cause an assertion failure");
+    protected ViewFactory createViewFactory() {
+        return new AndroidTestViewFactory(getContext());
     }
 
-    private FrameLayout wrapInRoot(View view) {
-        FrameLayout root = new FrameLayout(getContext());
-        root.addView(view);
-        return root;
-    }
-
-    public void testAssertViewExistsWithSingleView() {
-        TextView view = new TextView(getContext());
-        assertViewExists("TextView", wrapInRoot(view));
-    }
-
-    public void testFailingAssertViewExistsWithSingleView() {
-        try {
-            assertViewExists("FooView", wrapInRoot(new TextView(getContext())));
-            failHard();
-        } catch (AssertionError ex) {
-            // expected
-        }
-    }
-
-    public void testAssertViewCountWithSingleView() {
-        TextView view = new TextView(getContext());
-        View root = wrapInRoot(view);
-        assertViewCount("TextView", root, 1);
-        assertViewCount("FooView", root, 0);
-    }
-
-    public void testFailingAssertViewCountWithSingleView() {
-        try {
-            assertViewCount("TextView", wrapInRoot(new TextView(getContext())),  0);
-            failHard();
-        } catch (AssertionError ex) {
-            // expected
-        }
-    }
-
-    public void testAssertViewAttributesEqualWithSingleView() {
-        TextView view = new TextView(getContext());
-        view.setText("foo");
-        view.setTag("bar");
-        View root = wrapInRoot(view);
-        assertViewAttributesEqual("TextView", "text", root, "foo");
-        assertViewAttributesEqual("TextView", "tag", root, "bar");
-    }
-
-    public void testFailingAssertViewAttributesEqualWithSingleView() {
-        try {
-            assertViewAttributesEqual("TextView", "text", wrapInRoot(new TextView(getContext())), "foo");
-            failHard();
-        } catch (AssertionError ex) {
-            // expected
-        }
-    }
 }
---
+++ src/test-lib/src/com/nikhaldimann/viewselector/test/abstrakt/AbstractViewSelectorAssertsTest.java
@@ -0,0 +1,86 @@
+package com.nikhaldimann.viewselector.test.abstrakt;
+
+import static com.nikhaldimann.viewselector.ViewSelectorAsserts.assertViewAttributesEqual;
+import static com.nikhaldimann.viewselector.ViewSelectorAsserts.assertViewCount;
+import static com.nikhaldimann.viewselector.ViewSelectorAsserts.assertViewExists;
+import junit.framework.AssertionFailedError;
+
+import org.junit.Test;
+
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.nikhaldimann.viewselector.test.util.ViewSelectorAndroidTestCase;
+
+public abstract class AbstractViewSelectorAssertsTest extends ViewSelectorAndroidTestCase {
+
+    /**
+     * Fails with a RuntimeException, so we can distinguish this from planned
+     * assertion failures.
+     */
+    private void failHard() {
+        throw new RuntimeException("Failed to cause an assertion failure");
+    }
+
+    private FrameLayout wrapInRoot(View view) {
+        FrameLayout root = new FrameLayout(getContext());
+        root.addView(view);
+        return root;
+    }
+
+    @Test
+    public void testAssertViewExistsWithSingleView() {
+        TextView view = new TextView(getContext());
+        assertViewExists("TextView", wrapInRoot(view));
+    }
+
+    @Test
+    public void testFailingAssertViewExistsWithSingleView() {
+        try {
+            assertViewExists("FooView", wrapInRoot(new TextView(getContext())));
+            failHard();
+        } catch (AssertionFailedError ex) {
+            // expected
+        }
+    }
+
+    @Test
+    public void testAssertViewCountWithSingleView() {
+        TextView view = new TextView(getContext());
+        View root = wrapInRoot(view);
+        assertViewCount("TextView", root, 1);
+        assertViewCount("FooView", root, 0);
+    }
+
+    @Test
+    public void testFailingAssertViewCountWithSingleView() {
+        try {
+            assertViewCount("TextView", wrapInRoot(new TextView(getContext())),  0);
+            failHard();
+        } catch (AssertionFailedError ex) {
+            // expected
+        }
+    }
+
+    @Test
+    public void testAssertViewAttributesEqualWithSingleView() {
+        TextView view = new TextView(getContext());
+        view.setText("foo");
+        view.setTag("bar");
+        View root = wrapInRoot(view);
+        assertViewAttributesEqual("TextView", "text", root, "foo");
+        assertViewAttributesEqual("TextView", "tag", root, "bar");
+    }
+
+    @Test
+    public void testFailingAssertViewAttributesEqualWithSingleView() {
+        try {
+            assertViewAttributesEqual("TextView", "text", wrapInRoot(new TextView(getContext())), "foo");
+            failHard();
+        } catch (AssertionFailedError ex) {
+            // expected
+        }
+    }
+
+}
