cd6c2ac93a7cb529c1d56def1987ae3483b27e7d
Nik Haldimann <nhaldimann@gmail.com>
2013-06-17 15:51:07 -0400

    Transition fully to alpha-5 and fix warnings in Eclipse

--- src/test-lib/src/com/nikhaldimann/viewselector/test/SimpleFragment.java
+++ src/test-lib/src/com/nikhaldimann/viewselector/test/SimpleFragment.java
@@ -1,13 +1,16 @@
 package com.nikhaldimann.viewselector.test;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+@SuppressLint("NewApi")
 public class SimpleFragment extends Fragment {
 
+    @SuppressLint("NewApi")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedState) {
         super.onCreateView(inflater, container, savedState);
